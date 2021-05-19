b3588dbe62a71338fcc90d77d206e48132755933
Adrian Vazquez <aavm0000@gmail.com>
2021-05-19 10:09:39 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from _datetime import datetime
+from files_management import f_energy_boot
+from files_management import *
+from random import randint
+import time
+from post_http import post_scada
+if __name__ == "__main__":
+    print("Start App")
+    fake_rpi = [120,90]
+    METER_QUANTITY = 250
+    f_energy_boot(fake_rpi, METER_QUANTITY,"energy.json")
+    f_post_boot(fake_rpi, METER_QUANTITY,"post.json")
+    time_to_post = 120
+    while(1):
+        for lora_id in fake_rpi:
+            print("updating :", lora_id)
+            for slave in range(METER_QUANTITY):
+                energy_increment = randint(0, 100)
+                serial = lora_hex = (lora_id).to_bytes(2, "big")+(slave).to_bytes(1, 'big')
+                update_energy_file(serial, energy_increment)
+                update_post_file(serial,energy_increment)
+        post_scada("post.json",True)
+        for i in range(time_to_post):
+            print("Printing in :", time_to_post-i-1 ,"s")
+            time.sleep(1)       
---
+++ post_http.py
@@ -0,0 +1,86 @@
+import json
+import sys
+import logging
+import requests
+from distutils.log import debug
+
+log = logging.getLogger('post')
+ch = logging.NullHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+log.addHandler(ch)
+
+def post_json(file, is_production):
+    headers = {'Content-type': 'application/json'}
+    scada_url = 'https://postman-echo.com/post'
+    if is_production:
+        scada_url = "https://apimedidores.ciexpro.com/api/data/custom_create/" 
+    print(scada_url)
+    try:
+        r = requests.post(scada_url,
+                          json=file,
+                          headers=headers)
+        log.info("Status code is : %s", str(r.status_code))
+        print(r.json())
+        return r.status_code
+    except requests.exceptions.ConnectionError:
+        log.error("Connection Error!")
+    except Exception:
+        log.error("Problems? %s", str(sys.exc_info()))
+    return 0
+
+
+def post_scada(post_path, is_production=False):
+    log.info("Posting to Scada")
+    success_code =201
+    with open(post_path, 'r+') as post_file:
+        data_dic = json.load(post_file)
+        print(data_dic)
+        log.debug("Data to post: %s", str(data_dic))
+        r_code = post_json(data_dic, is_production)
+        print("Code: ", r_code)
+        if r_code == success_code:
+            print("Success")
+            with open("send_later.txt", "w+") as file:
+                lines = file.readlines()
+                count = 0
+                for line in lines:
+                    count += 1
+                    log.debug("Line{}: {}".format(count, line))
+                    dic = json.loads(line)
+                    post_json(dic)
+                file.seek(0)
+                file.truncate()
+
+        else:
+            log.error("Post unsuccessful")
+            text = json.dumps(data_dic) + "\n"
+            with open("send_later.txt", "a") as file:
+                file.write(text)
+            file.close()
+
+
+if __name__ == "__main__":
+
+    print("Start")
+    aux_dic = {
+        "id":
+        "0001",
+        "write_api_key":
+        "PYF7YMZNOM3TJVSM",
+        "updates": [{
+            "meterid": "000201",
+            "energy": 0,
+            "date": "2021-01-21 12:11:26.090530 -0400"
+        }, {
+            "meterid": "000202",
+            "energy": 0,
+            "date": "2021-01-21 12:11:26.090768 -0400"
+        }, {
+            "meterid": "000200",
+            "energy": 8,
+            "date": "2021-01-05 13:24:38.587472 -0400"
+        }]
+    }
+    post_scada(aux_dic)
